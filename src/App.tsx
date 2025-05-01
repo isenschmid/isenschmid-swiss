bf1abaa21ed68bf0bf2eb00208b19cc278655458
isenschmid <noah@azureee.ch>
2025-05-01 07:59:47 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -144,7 +144,7 @@ function App() {
                       : 'from-blue-900 to-blue-400'
                   } rounded-lg transform rotate-6 scale-105 opacity-50 blur-lg`}></div>
                   <img 
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" 
+                    src="https://github.com/isenschmid/isenschmid-swiss/blob/57d38c5fb12730ef451806917cf47b84010aa640/portfolio.jpg" 
                     alt="Professional headshot placeholder" 
                     className="relative z-10 w-full h-[400px] object-cover rounded-lg shadow-xl"
                   />
